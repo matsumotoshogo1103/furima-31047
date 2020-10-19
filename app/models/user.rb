deec7a3574ef73adde80114d707d4d5ddee972b3
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-19 17:52:28 +0900

    pry-railsの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
-  validates :birthday, presence: true
+  validates :birthday,       presence: true
 end
