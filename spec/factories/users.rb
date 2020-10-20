ffb34fcdd8402afe757a7bd8515c195b2202ecef
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-20 12:10:47 +0900

    FactoryBot編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :user do
+    Faker::Config.locale = :ja
     nickname              {Faker::Name.name}
-    last_name             {Faker::Name.last_name}
-    first_name            {Faker::Name.first_name}
-    last_name_kana        {Faker::Name.last_name}
-    first_name_kana       {Faker::Name.first_name}
+    last_name             {'田中'}
+    first_name            {'太郎'}
+    last_name_kana        {'タナカ'}
+    first_name_kana       {'タロウ'}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'test123'}
     password_confirmation {password}
     birthday              {Faker::Date.in_date_period}
   end
