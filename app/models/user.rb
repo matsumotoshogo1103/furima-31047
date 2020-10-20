bee869378a65563e4509c02c45fddfc9b4c03e43
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-20 14:34:38 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,6 @@ class User < ApplicationRecord
     with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
       validates :first_name
       validates :last_name
-      validates :first_name_kana
-      validates :last_name_kana
     end
 
     with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
