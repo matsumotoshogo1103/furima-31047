2f88f32a0491411108c7eaff7f8f5fad030b7db4
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-20 19:31:37 +0900

    active_hashのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :charge
+  belongs_to_active_hash :shipment_source
+  belongs_to_active_hash :shipment_date
+
 
   with_options presence: true do
     validates :name
