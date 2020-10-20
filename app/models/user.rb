cedfc51cadea9f2e2c00428b91fcae6f9dc31c8c
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-20 12:19:19 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday,       presence: true
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :first_name
+      validates :last_name
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+
+    with_options format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i } do
+      validates :password
+    end
+  end
 end
