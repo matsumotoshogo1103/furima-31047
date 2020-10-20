3cd9df54807505ce00ba3a3785156d84af8798a9
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-20 12:31:07 +0900

    rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
     Faker::Config.locale = :ja
-    nickname              {Faker::Name.name}
-    last_name             {'田中'}
-    first_name            {'太郎'}
-    last_name_kana        {'タナカ'}
-    first_name_kana       {'タロウ'}
-    email                 {Faker::Internet.free_email}
-    password              {'test123'}
-    password_confirmation {password}
-    birthday              {Faker::Date.in_date_period}
+    nickname              { Faker::Name.name }
+    last_name             { '田中' }
+    first_name            { '太郎' }
+    last_name_kana        { 'タナカ' }
+    first_name_kana       { 'タロウ' }
+    email                 { Faker::Internet.free_email }
+    password              { 'test123' }
+    password_confirmation { password }
+    birthday              { Faker::Date.in_date_period }
   end
 end
