3831e1c4429a2ec4d58f4849bb43d829b4108f5f
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-20 18:49:28 +0900

    categoryのactive_hash設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
 
   with_options presence: true do
     validates :name
