f67ecd246e2552c1825bc50b6acdc6c4d25fa5c1
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-22 14:33:22 +0900

    コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :destroy]
-  before_action :item_find, only: [:show, :destroy]
+  before_action :authenticate_user!, only: [:new, :destroy, :edit, :update]
+  before_action :item_find, only: [:show, :destroy, :edit, :update]
 
   def index
     @item = Item.order('id DESC')
@@ -20,6 +20,21 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if current_user.id == @item.user_id
+      @item.update(item_params)
+      redirect_to root_path
+    else
+      redirect_to item_path(@item.id)
+    end
   end
 
   def destroy
