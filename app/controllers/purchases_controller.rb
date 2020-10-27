44332948c6f118c836c96ec438a8d4321ff9ccd4
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-27 10:22:42 +0900

    カード決済機能（環境変数）

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,16 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [ :index, :create ]
+  before_action :item_find, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id
     @item_purchase = ItemPurchase.new
   end
 
   def create
-    binding.pry
-    @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new(item_purchase_params)
     if @item_purchase.valid?
+      pay_item
       @item_purchase.save
       redirect_to root_path
     else
@@ -22,6 +23,19 @@ class PurchasesController < ApplicationController
   def item_purchase_params
     params.require(:item_purchase).permit(:postal_code, :prefecture_id, :municipality, :street_number, :building, :phone)
     .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: item_purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def item_find
+    @item = Item.find(params[:item_id])
   end
 
 end
